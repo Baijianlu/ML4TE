49febc1789fe1fa1f223a57632307cce86bb3b30
Tao Fan <48915805+Baijianlu@users.noreply.github.com>
2024-05-04 11:49:26 +0300

    Add files via upload

---
+++ EnsembleLearn/P_type/AdaB/PRDF_10/learn.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score, GridSearchCV, StratifiedKFold
+from sklearn.metrics import classification_report
+import pickle
+
+if __name__ == '__main__':
+    '''This is for training the model! '''
+    df1 = pd.read_excel("train.xlsx", sheet_name='Sheet1',index_col=0)
+    df2 = pd.read_excel("test.xlsx", sheet_name='Sheet1',index_col=0)
+    Y_train = df1['class'].to_numpy()
+    Y_test = df2['class'].to_numpy()
+    df1.drop(columns=['class'],inplace=True)
+    df2.drop(columns=['class'],inplace=True) 
+    X_train = np.abs(df1.to_numpy())
+    X_test = np.abs(df2.to_numpy())
+    
+    dt_stump = DecisionTreeClassifier(min_weight_fraction_leaf=0.0022,class_weight='balanced')
+    estimators = [('scaler', StandardScaler()), ('clf', AdaBoostClassifier(base_estimator=dt_stump, random_state=0))] #
+    pipe = Pipeline(estimators)                     
+
+    param_grid = {"clf__n_estimators": [100,200,300,400,500,600,700,800], "clf__learning_rate": [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0], "clf__base_estimator__max_depth": [2,3,4,5,6,7]}
+    scores = ['f1', 'precision', 'recall']
+
+    cv = StratifiedKFold(n_splits=5)
+
+    grid_search = GridSearchCV(pipe, param_grid=param_grid, scoring=scores, refit='precision', cv=cv, n_jobs=4)
+
+    grid_search.fit(X_train, Y_train)
+    results_df = pd.DataFrame(grid_search.cv_results_)
+    print(grid_search.best_params_)
+    Y_pred = grid_search.predict(X_test)
+    print(classification_report(Y_test, Y_pred))
+    results_df.to_csv('cv_results.csv', index_label='index', sep="\t")
+    
+    ###save the model
+    pkl_model = "model.pkl"
+    with open(pkl_model, 'wb') as pklM:
+        pickle.dump(grid_search.best_estimator_, pklM)
+
+
+    
